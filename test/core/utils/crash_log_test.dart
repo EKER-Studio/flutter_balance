518dbc96e9c5bc21519f7fa8f39a5ecaebb90e30
Piotr Ekert <piotrekert90@gmail.com>
2026-08-15 17:41:00 +0200

    test: expand crashLogFileName validation in crash_log_test.dart

--- test/core/utils/crash_log_test.dart
+++ test/core/utils/crash_log_test.dart
@@ -2,8 +2,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:balance/core/utils/crash_log.dart';
 
 void main() {
-  group('crash_log', () {
-    test('crashLogFileName equals crash_log.txt', () {
+  group('crashLogFileName', () {
+    test('is a non-empty string', () {
+      expect(crashLogFileName, isA<String>());
+      expect(crashLogFileName, isNotEmpty);
+    });
+
+    test('has .txt extension', () {
+      expect(crashLogFileName, endsWith('.txt'));
+    });
+
+    test('contains crash_log prefix', () {
+      expect(crashLogFileName, startsWith('crash_log'));
+    });
+
+    test('is exactly crash_log.txt', () {
       expect(crashLogFileName, 'crash_log.txt');
     });
   });
